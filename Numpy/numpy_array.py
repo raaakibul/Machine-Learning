17310839e21cbf267411f855c652e0cce5dac694
rakibul-islam <raakibul.uap@gmail.com>
2025-06-26 22:35:28 +0600

    numpy array

---
+++ Numpy/numpy_array.py
@@ -0,0 +1,10 @@
+import numpy as np
+d1 = np.array(([1,2,3]))
+print(d1)
+print(d1.shape)
+d2 = np.array(([1,2,3],[4,5,6]))
+print(d2)
+print(d2.shape)
+d3 = np.array([[[1,2,3],[4,5,6]],[[7,8,9],[11,22,33]]])
+print(d3)
+print(d3.shape)
