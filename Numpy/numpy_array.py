9015ea465c6cb7c337c3a575754dab1a845245de
rakibul-islam <raakibul.uap@gmail.com>
2025-06-26 22:41:14 +0600

    numpy array

--- Numpy/numpy_array.py
+++ Numpy/numpy_array.py
@@ -8,3 +8,9 @@ print(d2.shape)
 d3 = np.array([[[1,2,3],[4,5,6]],[[7,8,9],[11,22,33]]])
 print(d3)
 print(d3.shape)
+
+print(np.array([range(1,5), range(1,5)]))
+print(np.arange(0,10,2))
+
+print(np.zeros((2,2)))
+print(np.ones((2,2), dtype='int'))
