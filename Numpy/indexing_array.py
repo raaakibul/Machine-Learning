7537039be26f5d16c44afc2f2b9ad2c4e37b6ef3
rakibul-islam <raakibul.uap@gmail.com>
2025-06-27 22:21:32 +0600

    array indeing

---
+++ Numpy/indexing_array.py
@@ -0,0 +1,13 @@
+import numpy as np 
+
+arr= np.array([[[1,2,3],[4,5,6]],[[7,8,9],[11,22,33]]])
+print(arr)
+print(arr.shape)
+print(arr[1,1])
+
+print(arr[0][1,2])
+print(arr[0,1,2])
+# arr = np.array(([1,2,3],[4,5,6]))
+# print(arr)
+# print(arr[0,2])
+# print(arr[1,1])
